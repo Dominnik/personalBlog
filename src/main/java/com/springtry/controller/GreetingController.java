c5de09a0f79241a5abcec3bd5863a1b0e22727e6
Andrey Kovalenko <dominnik77@gmail.com>
2020-03-23 22:16:06 +0400

    Trying to connect the db -postgres

--- src/main/java/com/springtry/controller/GreetingController.java
+++ src/main/java/com/springtry/controller/GreetingController.java
@@ -1,28 +1,46 @@
 package com.springtry.controller;
 
+import com.springtry.model.entity.Message;
+import com.springtry.model.repository.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(
-            name = "name",
-            required = false,
-            defaultValue = "World"
-    ) String name, Map<String, Object> model
-    ) {
-        model.put("name", name);
-        return "greeting";
-    }
+//    @GetMapping("/greeting")
+//    public String greeting(@RequestParam(
+//            name = "name",
+//            required = false,
+//            defaultValue = "World"
+//    ) String name, Map<String, Object> model
+//    ) {
+//        model.put("name", name);
+//        return "greeting";
+//    }
 
-    @GetMapping
-    public String main (Map<String, Object> model) {
-        model.put("main", "I think everything works fine!");
-        return "main";
-    }
+//    @GetMapping
+//    public String main(Map<String, Object> model) {
+//        Iterable<Message> messages = messageRepo.findAll();
+//
+//        model.put("messages", messages);
+//        return "main";
+//    }
+
+//    @PostMapping
+//    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+//        Message message = new Message(text, tag);
+//        messageRepo.save(message);
+//
+//        Iterable<Message> messages = messageRepo.findAll();
+//        model.put("messages", messages);
+//        return "main";
+//    }
 }
